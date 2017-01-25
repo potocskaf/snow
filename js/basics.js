d6184f422288d488e79a03b91f08e973a642c81d
potocskaf <potocskaf@cnw.hu>
2017-01-25 15:56:41 +0100

    return basics

---
+++ js/basics.js
@@ -0,0 +1,30 @@
+var a = '5';
+var b = '5';
+var c = Number(a) + Number(b); //10
+c = c - 2; //8
+if (c === 9) {
+    console.log('its nine!');
+}
+else if (c === 1) {
+    console.log('its one!');
+}
+else if (c === 3) {
+    console.log('its three!'); //tested and ok
+}
+else {
+    console.log("i dont know!"); //tested and ok
+}
+while (c > 0) {
+    console.log(c);
+    c = c - 1;
+}
+
+var count = 6;
+while (count <= 12) {
+    console.log(count);
+    if (count === 9) {
+        console.log("its nine!");
+    }
+    count = count + 3;
+}
+
